4855907debb24f63724fc2542d680161ed842667
Daria-Voloshanovska <voloshanovskadaria@gmail.com>
2025-06-22 22:36:44 +0200

    add code in StudentServiceImpl

---
+++ src/main/java/ait/cohort5860/student/dto/StudentUpdateDto.java
@@ -0,0 +1,12 @@
+package ait.cohort5860.student.dto;
+
+
+import lombok.Getter;
+
+
+@Getter
+public class StudentUpdateDto {
+
+    private String name;
+    private String password;
+}
---
+++ src/main/java/ait/cohort5860/student/service/StudentServiceApplication.java
@@ -0,0 +1,14 @@
+package ait.cohort5860.student.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StudentServiceApplication.class, args);
+    }
+
+}
